ce6827b91df300704ae06c52917bc34f1befbfd8
meenalgarg <meenalgarg2610@gmail.com>
2021-02-25 11:45:54 +0530

    adding jaeger

--- src/main/java/com/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.opentracing.Tracer;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,4 +20,10 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public Tracer initTracer(){
+		SamplerConfiguration samplerConfig = new SamplerConfiguration().withType("const").withParam(1);
+		ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true);
+		return Configuration.fromEnv("api-gateway").withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
+	}
 }
